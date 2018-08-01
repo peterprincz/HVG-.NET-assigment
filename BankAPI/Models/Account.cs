0e2dfd776670a4ca02623f9a76fe5394b1139a93
princz péter <princz.ptr@gmail.com>
2018-08-01 04:16:11 +0200

    Account abstract class added

---
+++ BankAPI/Models/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankAPI.Models
+{
+    public abstract class Account: IAccount
+    {
+
+        public Int32 id {get; private set; }
+
+        public string accountNumber { get; private set; }
+
+        public string owner { get; set; }
+
+        public decimal balance { get; private set; }
+
+        public string currency { get; set; }
+
+        public abstract bool isAmountWithdrawable();
+        public abstract void uploadMoney();
+        public abstract void withDraw(decimal amount);
+    }
+}
