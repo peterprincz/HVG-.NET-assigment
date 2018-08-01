8a667f3a3217d0dd9327cc50766bec08cf472075
princz péter <princz.ptr@gmail.com>
2018-08-01 04:15:45 +0200

    Account interface added

---
+++ BankAPI/Models/IAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankAPI.Models
+{
+    interface IAccount
+    {
+
+        bool isAmountWithdrawable();
+
+        void withDraw(decimal amount);
+
+        void uploadMoney();
+
+    }
+}
