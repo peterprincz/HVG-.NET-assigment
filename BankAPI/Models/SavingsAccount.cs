1514d0e14c2c1a298210ef5d0a8bff788b4ee753
princz péter <princz.ptr@gmail.com>
2018-08-01 04:15:03 +0200

    SavingAccount interface added

---
+++ BankAPI/Models/SavingsAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankAPI.Models
+{
+    public class SavingsAccount : Account
+    {
+
+        decimal interestRate;
+
+        public SavingsAccount(string decimal interestRate)
+        {
+            this.interestRate = interestRate;
+        }
+
+        public override bool isAmountWithdrawable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void uploadMoney()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void withDraw(decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
